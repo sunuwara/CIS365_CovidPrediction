346ab2e36a5756e7990d220f62f4e5ee29e9b638
sunuwara <sunuwara@mail.gvsu.edu>
2020-11-29 15:56:54 -0500

    separate each states data to own file - DONE

--- covid_pred_controller.py
+++ covid_pred_controller.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     """ Runs the controller for the Covid-19 Predictor app """
 
     # Initialize Model and View
-    model = CovidPredictorModel
-    view = CovidPredictorView
+    model = CovidPredictorModel()
+    view = CovidPredictorView()
 
     # Download raw data for latest Covid cases in US by state
     model.downloadData()
--- covid_pred_model.py
+++ covid_pred_model.py
@@ -1,8 +1,10 @@
 import requests
 import pandas as pd
 import numpy as np
+import csv
+import os.path
 
-mainFile = "./data/raw-us-states.csv"
+mainFilepath = "./data/raw-us-states.csv"
 
 
 class CovidPredictorModel:
@@ -16,8 +18,27 @@ class CovidPredictorModel:
         """ Gets Covid data for US states from: https://github.com/nytimes/covid-19-data """
         r = requests.get(
             'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
-        f = open(mainFile, 'w')
+        f = open(mainFilepath, 'w')
         f.write(str(r.content, 'utf-8'))
+        self.cleanData()
 
     def cleanData(self):
-        """ Clean the raw US states Covid cases data """
+        """ Clean the raw US states Covid cases data: split each data into different states """
+        f = open(mainFilepath)
+        with open(mainFilepath, 'r') as csvFile:
+            csvReader = csv.reader(csvFile)
+            firstLine = next(csvReader)
+
+            for line in csvReader:
+                newFilepath = "./data/" + line[1] + ".csv"
+
+                if not os.path.exists(newFilepath):
+                    with open(newFilepath, 'w') as newFile:
+                        # Open file normal mode to add column names to file
+                        csvWriter = csv.writer(newFile)
+                        csvWriter.writerow(firstLine)
+
+                with open(newFilepath, 'a+', newline='') as writeObj:
+                    # Open file in append mode to add following lines to file
+                    csvWriter = csv.writer(writeObj)
+                    csvWriter.writerow(line)
