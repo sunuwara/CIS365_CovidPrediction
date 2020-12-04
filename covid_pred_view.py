504caca354685dba89080b17d276136c65195fee
Karan Tamang <tamango@mail.gvsu.edu>
2020-12-03 22:36:01 -0500

    add prediction graph

--- covid_pred_view.py
+++ covid_pred_view.py
@@ -8,32 +8,36 @@ import numpy as np
 import pandas as pd
 import states as s
 
+
 app = dash.Dash()
 server = app.server
 
+# app layout
 app.layout = html.Div([
     html.H1("Covid Cases Analysis Dashboard", style={"textAlign": "center"}),
     html.Div([
+        # dropdown feature to select state
         dcc.Dropdown(id='case-dropdown',
                     options=[
                         # loop through each state
                         {'label': s.stateDict[i], 'value': i}
                         for i in s.stateDict
                     ],
-                    value='23',
+                    value='23', # default state-Michigan
                     placeholder="Select a state",
                     style={"margin-left": "auto", "margin-right": "auto", "width": "60%"}
                     ),
         dcc.Graph(id='case'),
 
         html.H1("Covid Deaths Analysis Dashboard", style={'textAlign': 'center'}),
+        # dropdown feature to select state
         dcc.Dropdown(id='death-dropdown',
                     options=[
                         # loop through each state
                         {'label': s.stateDict[i], 'value': i}
                         for i in s.stateDict
                     ],
-                    value='23',
+                    value='23', # default state-Michigan
                     placeholder="Select a state",
                     style={"margin-left": "auto", "margin-right": "auto", "width": "60%"}
                     ),
@@ -41,32 +45,47 @@ app.layout = html.Div([
     ], className="container"),
 ])
 
-''' Graph for covid-19 case for selected state'''
+''' Graph for covid-19 case for selected state '''
 @app.callback(Output('case', 'figure'),
               [Input('case-dropdown', 'value')])
 def update_graph1(selected_dropdown):
     trace1 = []
+    trace2 = []
+
+    # read the state name from state dictionary the read the csv file
     name = s.stateDict[selected_dropdown]
     temp = "./data/" + name + ".csv"
+    p_temp = "./predictions/" + name + "_cases.csv"
+    
+    # read the actual and predict csv file
     df = pd.read_csv(temp)
+    pf = pd.read_csv(p_temp)
 
     trace1.append(
         go.Scatter(x=df[df["state"] == name]["date"],
                     y=df[df["state"] == name]["cases"],
                     mode='lines', opacity=0.7,
-                    name='Cases ' + name, textposition='bottom center')
+                    name='Actual-Cases', textposition='bottom center')
     )
 
-    traces = [trace1]
+    trace2.append(
+        go.Scatter(x=pf[pf["state"] == name]["date"],
+                    y=pf[pf["state"] == name]["cases"],
+                    mode='lines', opacity=0.7,
+                    name='Predict-Cases', textposition='bottom center')
+    )
+
+    traces = [trace1, trace2]
     data = [val for sublist in traces for val in sublist]
 
+    # Graph data set and layout
     figure = {'data': data,
               'layout': go.Layout(
                   colorway=["#5E0DAC", '#FF4F00', '#375CB1',
                             '#FF7400', '#FFF400', '#FF0056'],
                   height=700,
                   title="Covid-19 Cases in " + name,
-                  xaxis={"title": "Date",
+                  xaxis={"title": "Date", "showgrid": False,
                          'rangeselector': {'buttons': list([
                              {'count': 1, 'label': '1D',
                               'step': 'day',
@@ -91,36 +110,51 @@ def update_graph1(selected_dropdown):
                               'stepmode': 'backward'},
                              {'step': 'all'}])},
                         },
-                  yaxis={"title": "Total Cases"})}
-
+                  yaxis={"title": "Total Cases", "showgrid": False})}
+  
     return figure
 
 ''' Graph for covid-19 death for selected state'''
 @app.callback(Output('death', 'figure'),
               [Input('death-dropdown', 'value')])
 def update_graph2(selected_dropdown):
+    trace1 = []
     trace2 = []
+    # read the state name from state dictionary
     name = s.stateDict[selected_dropdown]
     temp = "./data/" + name + ".csv"
-    df = pd.read_csv(temp)
+    p_temp = "./predictions/" + name + "_deaths.csv"
 
-    trace2.append(
+    # read the actual and predict csv file
+    df = pd.read_csv(temp)
+    pf = pd.read_csv(p_temp)
+
+    trace1.append(
         go.Scatter(x=df[df["state"] == name]["date"],
                     y=df[df["state"] == name]["deaths"],
                     mode='lines', opacity=0.7,
-                    name='Deaths ' + name, textposition='bottom center'
+                    name='Actual-Deaths', textposition='bottom center'
                     )
     )
-    traces = [trace2]
+    trace2.append(
+        go.Scatter(x=pf[pf["state"] == name]["date"],
+                    y=pf[pf["state"] == name]["deaths"],
+                    mode='lines', opacity=0.7,
+                    name='Predict-Deaths', textposition='bottom center'
+                    )
+    )
+
+    traces = [trace1, trace2]
     data = [val for sublist in traces for val in sublist]
 
+    # Graph data set and layout
     figure = {'data': data,
               'layout': go.Layout(
                 colorway=["#5E0DAC", '#FF4F00', '#375CB1',
                         '#FF7400', '#FFF400', '#FF0056'],
                 height=700,
                 title="Covid-19 Deaths in " + name,
-                xaxis={"title": "Date",
+                xaxis={"title": "Date", "showgrid": False,
                         'rangeselector': {'buttons': list([
                             {'count': 1, 'label': '1D',
                             'step': 'day',
@@ -145,11 +179,11 @@ def update_graph2(selected_dropdown):
                             'stepmode': 'backward'},
                             {'step': 'all'}])},
                     },
-                yaxis={"title": "Total Deaths"})}
+                yaxis={"title": "Total Deaths", "showgrid": False})}
 
     return figure
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=2021)
+    app.run_server(debug=True)
 
